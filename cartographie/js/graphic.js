9b461e8c89d2465c43d4d3cb464de54ebc247fb1
Samuel Goujon <samuel.goujon.fr@gmail.com>
2020-10-17 15:42:14 +0300

    .

--- cartographie/js/graphic.js
+++ cartographie/js/graphic.js
@@ -60,12 +60,9 @@ function renderChart() {
             '#fcaa17'
         ])
 
-        /* attrs.data.nodes.forEach(d => {
-          d.tag = religion ? 'image' : 'circle';
-          d.isImage = religion ? true : false;
-          d.imagePath = religion ? 'img/' + religion.filename : null;
-          d.radius = d.type === 'organization' ? attrs.radius_org : attrs.radius_people;
-        });*/
+        attrs.data.nodes.forEach(d => {
+          d.imagePath = 'img/judaism.svg';
+        });
 
         let scaleRadius = d3.scaleLinear()
             .domain([
