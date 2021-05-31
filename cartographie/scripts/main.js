41df59ca1e467a03e46ab6d3a4b526d2afbe3885
Samuel Lowe <samuel.goujon@yandex.com>
2021-05-31 09:32:51 +0300

    .

--- cartographie/scripts/main.js
+++ cartographie/scripts/main.js
@@ -48,8 +48,8 @@ function openNav(d) {
         if (summary && summary.description) {
           bioEl.innerHTML = summary.description;
         }
-      });
       wikipedia_url.innerHTML = "<a href=\"" + d.wikipedia + "\" target=\"_blank\">Wikipédia</a>";
+      });
     }
   } else {
     portrait.classList.add("d-none");
